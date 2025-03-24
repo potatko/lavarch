2c04e1e65d43123d9527d8401d2e980f05f52de4
potato <tomasko.jakubec@gmail.com>
2025-03-24 18:39:31 +0100

    updated

---
+++ src/projects.js
@@ -0,0 +1,66 @@
+export const projects = [
+    {
+        name: "BUNGALOV NATÁLIA",
+        year: "2018",
+        floors: 1,
+        rooms: 2,
+        area: 48,
+        type: "katalog",
+        image: "/assets/c-013.png"
+    },
+    {
+        name: "Vila tugendhat",
+        year: "2018",
+        floors: 2,
+        rooms: 4,
+        area: 120,
+        type: "individualny",
+        image: "/assets/rd-kb-013.png"
+    },
+    {
+        name: "Vila tugendhat",
+        year: "2018",
+        floors: 2,
+        rooms: 4,
+        area: 120,
+        type: "individualny",
+        image: "/assets/rd-kb-013.png"
+    },
+    {
+        name: "Vila tugendhat",
+        year: "2018",
+        floors: 2,
+        rooms: 4,
+        area: 120,
+        type: "individualny",
+        image: "/assets/rd-kb-013.png"
+    },
+    {
+        name: "Vila tugendhat",
+        year: "2018",
+        floors: 2,
+        rooms: 4,
+        area: 120,
+        type: "individualny",
+        image: "/assets/rd-kb-013.png"
+    },
+    {
+        name: "Vila tugendhat",
+        year: "2018",
+        floors: 2,
+        rooms: 4,
+        area: 120,
+        type: "individualny",
+        image: "/assets/rd-kb-013.png"
+    },
+    {
+        name: "Vila tugendhat",
+        year: "2018",
+        floors: 2,
+        rooms: 4,
+        area: 120,
+        type: "individualny",
+        image: "/assets/rd-kb-013.png"
+    },
+   
+];
